e7880b80ea1c1c825a11a39b5d06a4a5a04f00d3
Gergo Ignacz <ignaczg@KL-28657.local>
2025-09-22 10:03:22 +0300

    updates

--- keyboards/crkbd/keymaps/corne4gergo/keymap.c
+++ keyboards/crkbd/keymaps/corne4gergo/keymap.c
@@ -109,9 +109,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------------------------------------------------------------|        |--------------|        |-----------------------------------------------------|
       KC_ESC,              KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,        KC_PGDN,KC_HOME,        KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,
   //|--------+--------+--------+--------+--------+--------+--------|        |--------------|        |--------+--------+--------+--------+--------+--------|
-      TD(TD_LSFT_CAPS),    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        KC_PGDN,KC_END,        KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_BSLS,
+      KC_TAB,    KC_A,     KC_S,    KC_D,    KC_F,    KC_G,        KC_PGDN,KC_END,        KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_BSLS,
   //|--------+--------+--------+--------+--------+--------+--------|                                |--------+--------+--------+--------+--------+--------|
-      KC_TAB,              KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_QUOT,
+      TD(TD_LSFT_CAPS),    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_QUOT,
   //|--------+--------+--------+--------+--------+--------+--------|                                |--------+--------+--------+--------+--------+--------|
                                        KC_LALT,   MO(1),  GUI_T(KC_SPC),                      CTL_T(KC_ENT),   MO(2), KC_RALT
                                       //`--------------------------|                                |--------------------------|
@@ -121,9 +121,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------|                 |-----------------|         |-----------------------------------------------------.
       KC_ESC,      KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                 XXXXXXX, XXXXXXX,         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_DEL,
   //|--------+--------+--------+--------+--------+--------|                 |-----------------|         |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,     TD(TD_ASTR_DEG), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX,         KC_UNDS, KC_LEFT,   KC_UP,KC_RIGHT, XXXXXXX, KC_BSLS,
+      KC_TAB,     TD(TD_ASTR_DEG), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX,         KC_UNDS, KC_LEFT,   KC_UP,KC_RIGHT, XXXXXXX, KC_BSLS,
   //|--------+--------+--------+--------+--------+--------|                                             |--------+--------+--------+--------+--------+--------|
-      KC_TAB,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                         KC_PIPE, XXXXXXX, KC_DOWN, XXXXXXX, XXXXXXX, KC_QUOT,
+      KC_LSFT,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                         KC_PIPE, XXXXXXX, KC_DOWN, XXXXXXX, XXXXXXX, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------+--------|                                    |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LGUI, _______,  GUI_T(KC_SPC),                                    CTL_T(KC_ENT), KC_RALT,  MO(3)
                                       //|--------------------------|                                    |--------------------------|
@@ -133,9 +133,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.  ,---------.  ,-----------------------------------------------------.
        KC_ESC, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_AMPR,    XXXXXXX, XXXXXXX, KC_CIRC, KC_LPRN, KC_RPRN, XXXXXXX, XXXXXXX, KC_DEL,
   //|--------+--------+--------+--------+--------+--------|  |---------|  |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,  KC_TILD, KC_PCMM,  KC_PPLS, KC_PAST, KC_PERC,    XXXXXXX, XXXXXXX, KC_UNDS,  KC_LBRC, KC_RBRC, XXXXXXX, XXXXXXX,  KC_BSLS,
+      KC_TAB,  KC_TILD, KC_PCMM,  KC_PPLS, KC_PAST, KC_PERC,    XXXXXXX, XXXXXXX, KC_UNDS,  KC_LBRC, KC_RBRC, XXXXXXX, XXXXXXX,  KC_BSLS,
   //|--------+--------+--------+--------+--------+--------|              |--------+--------+--------+--------+--------+--------|
-      KC_TAB, XXXXXXX, KC_PDOT, KC_PMNS, KC_PSLS, XXXXXXX,                      KC_PIPE, KC_LCBR, KC_RCBR, XXXXXXX, XXXXXXX, KC_QUOT,
+      KC_LSFT, XXXXXXX, KC_PDOT, KC_PMNS, KC_PSLS, XXXXXXX,                      KC_PIPE, KC_LCBR, KC_RCBR, XXXXXXX, XXXXXXX, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LGUI,   MO(3),  GUI_T(KC_SPC),     CTL_T(KC_ENT), _______, KC_RALT
                                       //`--------------------------'  `--------------------------/
